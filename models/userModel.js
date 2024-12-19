b50b0451b4d9bec3ec79c3ef9548e11beed97e47
GuilhermeEijiY <guilherme-yoshida@hotmail.com>
2024-12-19 17:36:06 -0300

    .

--- models/userModel.js
+++ models/userModel.js
@@ -1,48 +1,61 @@
-// armazena a lista de usuários
-const users = [];
+const fs = require('fs');
+const path = require('path');
 
-let id = 1;
+// Caminho do arquivo JSON
+const filePath = path.join(__dirname, '../data/users.json');
 
-// retorna todos os usuários
+// Inicializa o arquivo JSON se não existir
+if (!fs.existsSync(filePath)) {
+    fs.writeFileSync(filePath, JSON.stringify([], null, 2));
+}
+
+// Carrega os usuários do arquivo JSON
+const users = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+
+let id = users.length > 0 ? Math.max(...users.map(user => user.id)) + 1 : 1;
+
+// Salva os dados no arquivo JSON
+const saveToFile = () => {
+    fs.writeFileSync(filePath, JSON.stringify(users, null, 2));
+};
+
+// Retorna todos os usuários
 exports.getAllUsers = () => users;
 
-// retorna um usuário baseado no id
+// Retorna um usuário baseado no id
 exports.getUserById = (id) => users.find(user => user.id === id);
 
-// adiciona um usuário ao array
+// Adiciona um usuário ao array
 exports.createUser = (user) => {
-    const newUser = { id, isAdmin: user.isAdmin || id, ...user };
+    const newUser = { id, isAdmin: user.isAdmin || false, ...user };
     users.push(newUser);
-
     id++;
-
+    saveToFile(); // Salva no arquivo
     return newUser;
 };
 
-
-// atualiza um usuário baseado em seu id
+// Atualiza um usuário baseado em seu id
 exports.updateUser = (id, updatedUser) => {
     const index = users.findIndex(user => user.id === id);
 
-    if(index === -1){
+    if (index === -1) {
         return null;
     }
 
-    // desestructuring: sobrescreve as chaves presentes em updatedUser
     users[index] = { ...users[index], ...updatedUser };
-
+    saveToFile(); // Salva no arquivo
     return users[index];
 };
 
-
-// deleta um usuário com base em seu id
+// Deleta um usuário com base em seu id
 exports.deleteUser = (id) => {
     const index = users.findIndex(user => user.id === id);
 
-    if(index === -1){
+    if (index === -1) {
         return null;
     }
 
-    // retorna o usuário deletado e o remove do array
-    return users.splice(index,1)[0];
+    const deletedUser = users.splice(index, 1)[0];
+    saveToFile(); // Salva no arquivo
+    return deletedUser;
 };
