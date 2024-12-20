a6032863106291a9ac2d8c842bcc4eadcf3ce4a3
Rafael Yuiti Kondo Bueno <rafaelkondobueno@gmail.com>
2024-12-20 16:25:34 -0300

    Paginação Cars e Rents

--- routes/rentalRoutes.js
+++ routes/rentalRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 // Rotas protegidas
 router.post('/', validateRental,authenticateToken, isAdmin, rentalController.createRental);
-router.get('/', authenticateToken, rentalController.getAllRentals);
+router.get('/', authenticateToken, rentalController.getAllRentals);                             // exemplo de requisição: /rentals?limite=10&pagina=3
 router.put('/:id', authenticateToken, isAdmin, rentalController.updateRental);
 router.delete('/:id', authenticateToken, isAdmin, rentalController.deleteRental);
 router.get('/:id', validateRentalId,authenticateToken, rentalController.getRentalById);
--- services/rentalService.js
+++ services/rentalService.js
@@ -1,7 +1,18 @@
 const rentalModel = require('../models/rentalModel');
 
 // Realiza as chamadas declaradas no rentalModel
-exports.getAllRentals = () => rentalModel.getAllRentals();
+exports.getAllRentalsPaginated = (limit, page) => {
+    const validLimits = [5, 10, 30];
+    if (!validLimits.includes(limit)) {
+        throw new Error(`Limite inválido. Valores permitidos: ${validLimits.join(', ')}`);
+    }
+
+    const rentals = rentalModel.getAllRentals();
+    const startIndex = (page - 1) * limit;
+    const endIndex = startIndex + limit;
+
+    return rentals.slice(startIndex, endIndex);
+};
 
 exports.getRentalById = (id) => rentalModel.getRentalById(id);
 
